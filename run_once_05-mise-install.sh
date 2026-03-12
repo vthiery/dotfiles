bb25b0cd5ea5fe27c1ed04b2b10c5db0a2714fe3
vthiery <vincent.thiery@camunda.com>
2026-03-12 16:57:45 +0100

    fix: use asdf-neovim plugin to avoid vfox GitHub 403

--- run_once_05-mise-install.sh
+++ run_once_05-mise-install.sh
@@ -3,6 +3,7 @@
 export PATH="$HOME/.local/bin:$PATH"
 
 # Register asdf plugins not in the default mise registry
+mise plugin add neovim https://github.com/richin13/asdf-neovim.git
 mise plugin add teleport-ent https://github.com/mise-plugins/mise-teleport-ent.git
 
 mise install
